5d31c433714c3aa36048d5d6b08dcbae5ff6b8a3
Craig Hobbs <craigahobbs@gmail.com>
2024-04-11 11:04:48 -0700

    remove licenses from empty __init__.py files

--- template/src/package-name/__init__.py
+++ template/src/package-name/__init__.py
@@ -1,10 +1,10 @@
+{%- if noapi is not defined or not noapi -%}
 # Licensed under the MIT License
 # https://github.com/{{github}}/{{package}}/blob/main/LICENSE
-{%- if noapi is not defined or not noapi %}
 
 """
 {{package}} package
 """
 
 from .{{ package | replace('-', '_') }} import sum_numbers
-{%- endif %}
+{% endif -%}
--- test-expected/nomain/src/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# Licensed under the MIT License
-# https://github.com/johndoe/my-package/blob/main/LICENSE
